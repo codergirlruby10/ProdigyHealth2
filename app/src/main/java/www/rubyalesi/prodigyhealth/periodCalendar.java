bb03f0f5296bfa89a18ca6831a647cd5d5a38fa2
Ruby Alesi <rubyalesi@icloud.com>
2021-05-14 07:04:17 -0700

    Added link to Calendar

--- app/src/main/java/www/rubyalesi/prodigyhealth/periodCalendar.java
+++ app/src/main/java/www/rubyalesi/prodigyhealth/periodCalendar.java
@@ -1,14 +1,25 @@
 package www.rubyalesi.prodigyhealth;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class periodCalendar extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_period_calendar);
+        setupHyperLink();
+
+
+    }
+
+    private void setupHyperLink() {
+        TextView linkTextView = findViewById(R.id.periodCalendar_main_link);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
